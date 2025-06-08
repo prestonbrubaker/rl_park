ccfd658bf2704650ed745a64b2ecf77ba376bc26
Willoh Brubaker <willohbrubaker@outlook.com>
2025-06-08 14:10:18 -0500

    m

--- willfriend/willfriend.py
+++ willfriend/willfriend.py
@@ -6,14 +6,14 @@ pygame.init()
 font = pygame.font.SysFont("roboto", 24)
 
 
-window = pygame.display.set_mode((800,800))
+window = pygame.display.set_mode((1600,1600))
 height = window.get_height()
 width = window.get_width()
 print(height)
 
 scroll_y = 0
 
-dt = 0.008
+dt = 0.018
 g = 20
 WHITE = (255, 255, 255)
 
@@ -21,8 +21,11 @@ platforms = []
 platform_w = 100
 platform_h = 20
 
-score = 0
+ground_h = 100
 
+score = 0
+max_alt = 0
+plat_pen = 0
 
 class Player:
     def __init__(self, x, y, vel_y):
@@ -35,15 +38,16 @@ class Player:
 def check_boundries():
     global scroll_y
     global score
+    global plat_pen
     on_plat = False
     for platform in platforms:
-        if player.x > platform[0] and player.x < platform[0] + platform_w and player.y + 50 > platform[1] + scroll_y and player.y + 50 < platform[1] + platform_h + scroll_y and player.vel_y > 0:
+        if (player.x + 25) > platform[0] and (player.x + 25) < platform[0] + platform_w and player.y + 50 > platform[1] + scroll_y and player.y + 50 < platform[1] + platform_h + scroll_y and player.vel_y > 0:
             player.on_ground = True
             on_plat =True
             player.vel_y = 0
             player.y = platform[1] - 50 + scroll_y
             if platform[2] == 1:
-                score += 1
+                plat_pen += 1
                 platform[2] = 0
 
     if player.x < 0:
@@ -72,17 +76,32 @@ def apply_gravity():
 
 def physic():
     global scroll_y
+    global max_alt
+    global plat_pen
+    global score
     player.y += player.vel_y * dt
     if player.y < 0:
         scroll_y -= player.y
         player.y = 0
+    if height - player.y + scroll_y > max_alt:
+        max_alt = height - player.y + scroll_y
+        score = round((max_alt - plat_pen * 100) / 100, 2)
+    score = round((max_alt - plat_pen * 100) / 100, 2)
 
 def create_platform(i):
-    platforms.append([
+    if not i:   
+        platforms.append([
+                random.uniform(0, width),
+                random.uniform(height - 190 - ground_h, height - ground_h),
+                1
+        ])
+    else:
+        platforms.append([
             random.uniform(0, width),
-            random.uniform(-200 * i, height),
+            platforms[-1][1] - random.uniform(0, 190),
             1
-    ])
+        ])
+
 
 def update_platforms():
     for platform in platforms:
@@ -90,14 +109,14 @@ def update_platforms():
         platform[0] += dx
         if platform[0] < 0:
             platform[0] = 0
-        if platform[0] > width:
-            platform[0] = width
+        if platform[0] > width - platform_w:
+            platform[0] = width - platform_w
         
 
 
-player = Player(x=0,y=650, vel_y=0)
+player = Player(x=0,y=height - 50 - ground_h, vel_y=0)
 
-for i in range(1000):
+for i in range(1300):
     create_platform(i)
 
 running = True
@@ -109,12 +128,12 @@ while running:
     keys = pygame.key.get_pressed()
     # if keys[pygame.K_w]:
     #     player.y -= 300 * dt
-    # if keys[pygame.K_s]:
-    #     player.y += 300 * dt
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        player.y += 300 * dt
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        player.x -= 100 * dt
+        player.x -= 200 * dt
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        player.x += 100 * dt
+        player.x += 200 * dt
     if keys[pygame.K_SPACE] and player.on_ground:
         player.vel_y -= 100
         player.on_ground = False
@@ -127,7 +146,7 @@ while running:
     
     window.fill((0,0,0))
     pygame.draw.rect(window,(255,0,0),(player.x,player.y,50,50))
-    pygame.draw.rect(window, (255, 255, 0), (0,700 + scroll_y,800,100))
+    pygame.draw.rect(window, (255, 255, 0), (0,height - ground_h + scroll_y,width,ground_h))
     position_text = font.render(f'X: {int(player.x)}   Y: {int(player.y)}   ON PLAT: {player.on_ground}     SCORE: {score}', True, WHITE)
     window.blit(position_text, (10, 10))
     for platform in platforms:
